6185dfbafe71a5413f2a1c89aa1f66f9aa4c6b20
0429oonishi <oonishireon110@gmail.com>
2021-07-04 23:27:24 +0900

    ボタンを配置

--- Animation-Sample/ViewController.swift
+++ Animation-Sample/ViewController.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    
+    @IBAction private func buttonDidTapped(_ sender: Any) {
+        let animation = CABasicAnimation(keyPath: <#T##String?#>)
     }
-
-
+    
+    
 }
 
